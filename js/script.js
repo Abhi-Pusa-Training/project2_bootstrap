3d568a1a3eea4e96f7cf75da8eae85439844ea13
abhinavtdgp <abhinav.ashesh@wipro.com>
2015-12-15 12:34:38 +0530

    commited successfully

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+
+$(function() {
+
+//alert('this is the javascript page');
+	$("#alert_Me").click(function() {
+			//e.preventDefault();
+			$("#Success_Alert").slideDown();
+	  });
+
+	/*	$( "#alert_Me" ).click(function() {
+  $('#Success_Alert').slideDown();
+});*/
+		});
